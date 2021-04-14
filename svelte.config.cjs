5e7aab7c09e118e05354fb4d44553d8fe58c2531
Alex Rock Ancelet <pierstoval@gmail.com>
2021-04-14 21:21:21 +0200

    Init Svelte-kit

---
+++ svelte.config.cjs
@@ -0,0 +1,28 @@
+const sveltePreprocess = require('svelte-preprocess');
+//const nodeAdapter = require('@sveltejs/adapter-node');
+const staticAdapter = require('@sveltejs/adapter-static');
+const pkg = require('./package.json');
+
+/** @type {import('@sveltejs/kit').Config} */
+module.exports = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: sveltePreprocess(),
+
+	kit: {
+		// By default, `npm run build` will create a standard Node app.
+		// You can create optimized builds for different platforms by
+		// specifying a different adapter
+		// adapter: nodeAdapter(),
+		adapter: staticAdapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		vite: {
+			ssr: {
+				noExternal: Object.keys(pkg.dependencies || {})
+			}
+		}
+	}
+};
